c710904f64ae507f5e490a392dba16b6ff4fd7b3
Markus Felten <Markus.Felten@gmx.de>
2021-02-16 09:45:08 +0100

    fix: parse csv

--- src/konsum-import-cli.mjs
+++ src/konsum-import-cli.mjs
@@ -1,20 +1,31 @@
 #!/usr/bin/env node
 import { createReadStream } from "fs";
+import csv from "csv-parser";
+
+/*
 import levelup from "levelup";
 import leveldown from "leveldown";
 import { Master, VALUE_PREFIX, secondsAsString } from "konsum-db";
-
+*/
 /**
  */
-async function execute(csvFile="sunnyBoy-201703.csv",dbFile="db") {
+async function execute(
+  csvFile = "tests/fixtures/sunnyBoy-201703.csv",
+  dbFile = "db",
+  categoryName = "sunny"
+) {
+  const parser = csv({
+    separator: ";",
+    skipLines: 9,
+    headers: ["date", "total", "increment"]
+  });
+  createReadStream(csvFile, { encoding: "utf8" }).pipe(parser);
 
-  const stream = createReadStream(csvFile,{encoding:"utf8"});
-
-  for(const chunk of stream) {
-    console.log(chunk);
+  for await (const record of parser) {
+    console.log(record);
   }
 
-/*
+  /*
   const master = await Master.initialize(await levelup(leveldown(dbFile)));
       let nKey = m[1] + secondsAsString(Math.round(parseFloat(m[2])));
 
